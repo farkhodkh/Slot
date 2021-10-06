b28e0ee3dca43a7955883bd2cdf4b984c4ac242a
farkhod <986532qw@gmail.com>
2021-10-06 22:10:20 +0300

    Initial Commit

---
+++ Slot/SlotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotApp.swift
+//  Slot
+//
+//  Created by Farkhod on 06.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
